051362e9b386d1dddc0d9dfba4734a6fc24f647a
James Halliday <mail@substack.net>
2014-10-31 16:12:00 -0700

    as a library

--- example/server.js
+++ example/server.js
@@ -4,20 +4,13 @@ var url = require('url');
 var fs = require('fs');
 var stdir = ecstatic(__dirname + '/static');
 
+var appver = require('../')();
+
 var server = http.createServer(function (req, res) {
     console.error(req.method, req.url);
     
-    var u = url.parse(req.url);
-    if (u.pathname === '/site.appcache') {
-        var age = 60 * 60 * 24 * 365.25 * 100; // ~100 years
-        res.setHeader('cache-control', 'max-age=' + age);
-        res.setHeader('content-type', 'text/cache-manifest; charset=UTF-8');
-        var file = __dirname + '/static/site.appcache';
-        fs.createReadStream(file).pipe(res);
-    }
-    else  {
-        stdir(req, res);
-    }
+    if (appver.exec(req, res)) return;
+    else stdir(req, res);
 });
 server.listen(0, function () {
     console.error('http://localhost:' + server.address().port);
---
+++ index.js
@@ -0,0 +1,65 @@
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var defined = require('defined');
+var routes = require('routes');
+var rprefix = require('route-prefix');
+
+module.exports = AppVer;
+
+function AppVer (opts) {
+    if (!(this instanceof AppVer)) return new AppVer(opts);
+    if (!opts) opts = {};
+    this.prefix = path.resolve('/', defined(opts.prefix, '/'));
+    this.router = this._createRoutes(); //rprefix(this.prefix, this._createRoutes());
+    this.maxage = defined(
+        opts.maxage,
+        Math.floor(60 * 60 * 24 * 365.25 * 100) // ~100 years
+    );
+}
+
+AppVer.prototype.exec = function (req, res) {
+    var m = this.router.match(req.url);
+    if (!m) return null;
+    m.fn(req, res, m);
+    return true;
+};
+
+AppVer.prototype._createRoutes = function () {
+    var self = this;
+    var r = routes();
+    r.addRoute('/', function (req, res, params) {
+        res.setHeader('content-type', 'text/html; charset=UTF-8');
+        res.setHeader('cache-control', 'max-age=' + self.maxage);
+        readFile('index.html').pipe(res);
+    });
+    r.addRoute('/appver.js', function (req, res, params) {
+        res.setHeader('content-type', 'text/javascript; charset=UTF-8');
+        res.setHeader('cache-control', 'max-age=' + self.maxage);
+        readFile('appver.js').pipe(res);
+    });
+    r.addRoute('/appver.appcache', function (req, res, params) {
+        res.setHeader('cache-control', 'max-age=' + self.maxage);
+        res.setHeader('content-type', 'text/cache-manifest; charset=UTF-8');
+        
+        res.end('CACHE MANIFEST\n'
+            + path.join(self.prefix) + '\n'
+            + path.join(self.prefix, 'appver.appcache') + '\n'
+            + path.join(self.prefix, 'appver.js') + '\n'
+            + 'NETWORK:\n'
+            + 'versions.json\n'
+        );
+    });
+    r.addRoute('/versions.json', function (req, res, params) {
+        res.setHeader('cache-control', 'max-age=' + self.maxage);
+        res.setHeader('content-type', 'application/json');
+        res.write('{"0.0.0":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}\n');
+        res.end();
+    });
+    return r;
+};
+
+function readFile (file) {
+    var fpath = path.join(__dirname, 'static', file);
+    return fs.createReadStream(fpath);
+}
