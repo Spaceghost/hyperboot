81b12fcdaf865b437b412c7940b0ee35c65ff407
James Halliday <mail@substack.net>
2014-11-01 02:24:33 -0700

    bootdir-finder

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,6 +6,7 @@ var minimist = require('minimist');
 var defined = require('defined');
 var mkdirp = require('mkdirp');
 var tmpdir = require('osenv').tmpdir;
+var parents = require('parents');
 
 var argv = minimist(process.argv.slice(2), {
     alias: {
@@ -18,22 +19,23 @@ var argv = minimist(process.argv.slice(2), {
         rdir: defined(process.env.BOOTVER_RELEASEDIR, 'bootver')
     }
 });
-var dir = defined(argv.dir, path.join(process.cwd(), argv.rdir));
 
 if (argv._[0] === 'help' || argv.help || argv.h) {
     usage(0);
 }
 else if (argv._[0] === 'release') {
-    mkdirp.sync(dir);
-    var input = defined(argv._[1], '-') === '-'
-        ? process.stdin
-        : fs.createReadStream(argv._[1])
-    ;
-    var release = require('./release.js');
-    input.pipe(release(dir, argv, function (err, hex) {
-        if (err) error(err)
-        else console.log(hex)
-    }));
+    withDir(function (dir) {
+        mkdirp.sync(dir);
+        var input = defined(argv._[1], '-') === '-'
+            ? process.stdin
+            : fs.createReadStream(argv._[1])
+        ;
+        var release = require('./release.js');
+        input.pipe(release(dir, argv, function (err, hex) {
+            if (err) error(err)
+            else console.log(hex)
+        }));
+    });
 }
 else usage(1);
 
@@ -48,4 +50,17 @@ function usage (code) {
     r.on('end', function () {
         if (code) process.exit(code);
     });
+}
+
+function withDir (cb) {
+    if (argv.dir) return cb(argv.dir);
+    var dirs = parents(process.cwd());
+    (function next () {
+        if (dirs.length === 0) return cb(path.join(process.cwd(), argv.rdir));
+        var dir = dirs.shift();
+        fs.stat(path.join(dir, argv.rdir), function (err) {
+            if (err) next()
+            else cb(path.join(dir, argv.rdir))
+        });
+    })();
 }
