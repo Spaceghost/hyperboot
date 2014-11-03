6a003dcb1765fd610cfdc116012614758e0b2561
Eugene Sharygin <eush77@gmail.com>
2014-11-03 23:57:58 +0300

    Check if the version has been already released

--- bin/release.js
+++ bin/release.js
@@ -11,27 +11,43 @@ module.exports = function (dir, opts, cb) {
         tmpdir(), '.hyperboot-' + Date.now() + '-' + Math.random()
     );
     var jsonfile = path.join(dir, 'versions.json');
+    var input = through();
     if (!opts.version) {
         process.nextTick(function () {
             cb(new Error('version not provided'));
         });
-        return through();
+        return input;
     }
-    
-    var hex, pending = 2;
-    var w = fs.createWriteStream(tmpfile);
-    w.on('finish', function () { if (-- pending === 0) done() });
-    var h = createHash('sha256');
-    h.pipe(concat(function (body) {
-        hex = body.toString('hex');
-        if (-- pending === 0) done();
-    }));
-    
-    var input = through();
-    input.pipe(w);
-    input.pipe(h);
+
+    var versions, hex;
+    fs.readFile(jsonfile, function (err, body) {
+        if (err && err.code === 'ENOENT') body = Buffer('[]');
+        else if (err) return cb(err);
+
+        try { versions = JSON.parse(body.toString('utf8')) }
+        catch (err) { return cb(err) }
+
+        if (versions.some(function (ref) {
+            return ref.version === opts.version;
+        })) {
+          return cb(new Error('cannot modify pre-existing version: ' + opts.version));
+        }
+
+        var pending = 2;
+        var w = fs.createWriteStream(tmpfile);
+        w.on('finish', function () { if (-- pending === 0) done() });
+        var h = createHash('sha256');
+        h.pipe(concat(function (body) {
+          hex = body.toString('hex');
+          if (-- pending === 0) done();
+        }));
+
+        input.pipe(w);
+        input.pipe(h);
+    });
+
     return input;
-    
+
     function done () {
         var dstfile = path.join(dir, hex + '.html');
         fs.rename(tmpfile, dstfile, function (err) {
@@ -39,25 +55,17 @@ module.exports = function (dir, opts, cb) {
             else updateJson()
         });
     }
-    
+
     function updateJson () {
-        fs.readFile(jsonfile, function (err, body) {
-            if (err && err.code === 'ENOENT') body = Buffer('[]');
-            else if (err) return cb(err);
-            
-            try { var versions = JSON.parse(body.toString('utf8')) }
-            catch (err) { return cb(err) }
-            
-            var ref = { hash: hex };
-            if (opts.version) ref.version = opts.version;
-            if (opts.message) ref.message = opts.message;
-            
-            versions.push(ref);
-            var src = JSON.stringify(versions, null, 2) + '\n';
-            fs.writeFile(jsonfile, src, function (err) {
-                if (err) cb(err)
-                else cb(null, hex)
-            });
+        var ref = { hash: hex };
+        if (opts.version) ref.version = opts.version;
+        if (opts.message) ref.message = opts.message;
+
+        versions.push(ref);
+        var src = JSON.stringify(versions, null, 2) + '\n';
+        fs.writeFile(jsonfile, src, function (err) {
+            if (err) cb(err)
+            else cb(null, hex)
         });
     }
 };
