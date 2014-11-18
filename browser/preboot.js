74b71abe138954354a71806abec6461213fd68ca
James Halliday <mail@substack.net>
2014-11-19 00:41:40 +0100

    boot version maps to boot hash, all under the application name

--- browser/preboot.js
+++ browser/preboot.js
@@ -1,12 +1,21 @@
-var bootsrc = localStorage.getItem('hyperboot!boot');
-if (bootsrc) {
+var nameElem = document.querySelector('*[data-name]');
+var appname = nameElem && nameElem.textContent;
+
+var boothash = localStorage.getItem('hyperboot!' + appname + '!boot');
+if (boothash) {
+    var bootsrc = localStorage.getItem('hyperboot!' + appname + '!' + boothash);
+    if (!bootsrc) {
+        console.error('BOOTLOADER VERSION NOT FOUND: ' + boothash);
+        console.error('USING BOOTLOADER BACKUP');
+        backup();
+    }
     try {
         eval(bootsrc)
     }
     catch (err) {
         console.error('ERROR LOADING BOOTLOADER:');
         console.error(err);
-        console.error('USING LAST WORKING BACKUP');
+        console.error('USING BOOTLOADER BACKUP');
         backup();
     }
 }
