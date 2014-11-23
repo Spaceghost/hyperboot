553ed684301a15e53528a278afb7f00ef1463345
James Halliday <mail@substack.net>
2014-11-23 07:47:46 -0800

    expose rpc options on the rpc interface

--- rpc.js
+++ rpc.js
@@ -3,8 +3,16 @@ var rpc = RPC(window, window.parent, '*');
 
 exports.rpc = rpc;
 
-exports.configure = function () {
-    rpc.apply('configure', [].slice.call(arguments));
+exports.show = function () {
+    rpc.apply('show', [].slice.call(arguments));
+};
+
+exports.hide = function () {
+    rpc.apply('hide', [].slice.call(arguments));
+};
+
+exports.toggle = function () {
+    rpc.apply('toggle', [].slice.call(arguments));
 };
 
 // future more stuff! install, search, launch another program
