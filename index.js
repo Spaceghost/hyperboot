f0e609aec667c8cf868074ef504bc32f61376f6a
James Halliday <mail@substack.net>
2015-09-17 15:37:30 -0700

    emit current version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var url = require('url')
 var inherits = require('inherits')
 var EventEmitter = require('events').EventEmitter
 var hver = require('html-version')
+var semver = require('semver')
 
 module.exports = Hyperboot
 inherits(Hyperboot, EventEmitter)
@@ -11,19 +12,26 @@ function Hyperboot () {
   if (!(this instanceof Hyperboot)) return new Hyperboot
 }
 
-Hyperboot.prototype.load = function (href, cb) {
+Hyperboot.prototype.load = function (uhref, cb) {
   var self = this
+  var href = url.resolve(location.href, uhref)
   xhr(href, function (err, res, body) {
     if (err) return cb(err)
     var info = hver.parse(body)
     var versions = info.versions || {}
 
-    Object.keys(versions).forEach(function (key) {
+    Object.keys(versions).sort(semver.compare).forEach(function (key) {
       self.emit('version', {
         version: key,
         integrity: info.integrity[key] || [],
         hrefs: versions[key]
       })
+    })
+
+    self.emit('version', {
+      version: info.version,
+      integrity: info.integrity[href] || [],
+      hrefs: [ href ]
     })
   })
 }
