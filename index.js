8b505da5d61597aba21edf5ad681d5484b5fe4ab
James Halliday <mail@substack.net>
2015-09-27 12:13:31 -0700

    lexicographic version packer

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ var inherits = require('inherits')
 var EventEmitter = require('events').EventEmitter
 var hver = require('html-version')
 var semver = require('semver')
+var lex = require('lexicographic-integer')
 
 module.exports = Hyperboot
 inherits(Hyperboot, EventEmitter)
 
-function Hyperboot () {
-  if (!(this instanceof Hyperboot)) return new Hyperboot
+function Hyperboot (db) {
+  if (!(this instanceof Hyperboot)) return new Hyperboot(db)
+  this.db = db
+  if (db) {
+    this.on('version', function (v) {
+      db.put('version!' + packv(v.version)
+    })
+  }
 }
 
 Hyperboot.prototype.load = function (href, cb) {
@@ -40,4 +47,22 @@ Hyperboot.prototype.load = function (href, cb) {
 
 Hyperboot.prototype.versions = function (cb) {
   // ...
+}
+
+function packv (ver) {
+  var parts = ver.split('-')[0].split('.')
+  return hexa(lex.pack(parts[0]))
+    + '.' + hexa(lex.pack(parts[1]))
+    + '.' + hexa(lex.pack(parts[2]))
+    + ver.replace(/^\d+\.\d+\.\d+/, '')
+}
+
+function hexa (bytes) {
+  var res = []
+  for (var i = 0; i < bytes.length; i++) {
+    var h = bytes[i].toString(16)
+    if (h.length === 1) h = '0' + h
+    res.push(h)
+  }
+  return res.join('')
 }
